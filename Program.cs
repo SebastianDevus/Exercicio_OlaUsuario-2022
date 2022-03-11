0f988954b66fcef04a74fe84837be6af3a030a66
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-11 20:44:02 -0300

    Finalização da atividade

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string nome; // Declarando string
+
+Console.Write("Digite o seu nome: ");
+nome = Console.ReadLine()!; // Lendo o que o usuário digita e armazenando na string nome
+
+Console.WriteLine($"Olá, {nome}!"); // Exibindo o conteúdo da string nome após o Olá
